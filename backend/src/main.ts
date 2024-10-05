85d9d143f85244adb892365ccf7e484d8722d0f9
Manish Yadav <manish.yadav@softwareworkshop.net>
2024-10-06 03:55:29 +0530

    add docker-compose

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,16 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-  await app.listen(3001);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  const configService = app.get(ConfigService);
+  const port = configService.get('BACKEND_PORT') || 3001;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
